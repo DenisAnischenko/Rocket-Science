d1765de1a113a0bd84c2ff8e5b198f60e5480cf2
Stanislav Ivashkevich <stiv@relesys.net>
2020-11-23 12:14:08 +0300

    add program, add faq

--- script.js
+++ script.js
@@ -3,9 +3,23 @@
 let menu = document.querySelector('.menu');
 
 function toggleMenu() {
-    if (menu.className.indexOf('open') === -1) {
-        menu.classList.add('open');
+    toggleOpen(menu)
+}
+
+function toggleAccordion(el) {
+    let accordions = document.querySelectorAll('.accordion-wrapper');
+
+    accordions.forEach((accordion) => {
+        el !== accordion && accordion.classList.remove('opened');
+    })
+
+    toggleOpen(el);
+}
+
+let toggleOpen = (el) => {
+    if (el.className.indexOf('opened') === -1) {
+        el.classList.add('opened');
     } else {
-        menu.classList.remove('open');
+        el.classList.remove('opened');
     }
 }
