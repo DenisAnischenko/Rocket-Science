8abad4a4572cef7bf4687abec8d334d3fe7acb2d
Stanislav Ivashkevich <stiv@relesys.net>
2020-12-04 16:29:42 +0300

    add pricing, add slider

--- script.js
+++ script.js
@@ -1,25 +1,63 @@
-'use strict';
+(() => {
+    'use strict';
 
-let menu = document.querySelector('.menu');
+    window.onload = () => {
+        window.toggleMenu = toggleMenu;
+        window.toggleAccordion = toggleAccordion;
 
-function toggleMenu() {
-    toggleOpen(menu)
-}
-
-function toggleAccordion(el) {
-    let accordions = document.querySelectorAll('.accordion-wrapper');
-
-    accordions.forEach((accordion) => {
-        el !== accordion && accordion.classList.remove('opened');
-    })
-
-    toggleOpen(el);
-}
-
-let toggleOpen = (el) => {
-    if (el.className.indexOf('opened') === -1) {
-        el.classList.add('opened');
-    } else {
-        el.classList.remove('opened');
+        initSlider();
     }
-}
+
+    let menu = document.querySelector('.menu');
+
+    let toggleMenu = () => toggleOpen(menu);
+
+    let toggleAccordion = (el) => {
+        let accordions = document.querySelectorAll('.accordion-wrapper');
+
+        accordions.forEach((accordion) => {
+            el !== accordion && accordion.classList.remove('opened');
+        })
+
+        toggleOpen(el);
+    }
+
+    let toggleOpen = (el) => {
+        if (el.className.indexOf('opened') === -1) {
+            el.classList.add('opened');
+        } else {
+            el.classList.remove('opened');
+        }
+    }
+
+
+    let initSlider = () => {
+        let currentSlide = 0,
+            sliderWrapper = document.querySelector('.slider-wrapper'),
+            slides = document.querySelectorAll('.slider .slide'),
+            slideCount = slides.length || 1,
+            slideWidth = slides[0].offsetWidth,
+            prev = document.querySelector('.nav.left'),
+            next = document.querySelector('.nav.right');
+
+        sliderWrapper.style.width = 100 * slideCount + '%';
+
+        prev.addEventListener('click', clickPrev)
+        next.addEventListener('click', clickNext)
+
+        function clickPrev() {
+            currentSlide = currentSlide > 0 ? --currentSlide : (slideCount - 1);
+            calculateTransform(currentSlide);
+        }
+
+        function clickNext() {
+            currentSlide = currentSlide < (slideCount - 1) ? ++currentSlide : 0;
+            calculateTransform(currentSlide);
+        }
+
+        function calculateTransform(ind) {
+            sliderWrapper.style.transform = `translateX(-${ind * slideWidth}px)`
+        }
+
+    }
+})()
